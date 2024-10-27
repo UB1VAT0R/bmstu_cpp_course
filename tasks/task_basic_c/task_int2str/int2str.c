abf664e3acbf393364b1ad90d49a2e9a218d6b7e
ques <stetsenkoda1603@gmail.com>
2024-10-27 21:27:58 +0300

    complete

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,35 @@
 #include <assert.h>
-#include "int2str.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-char* int2str(int number) {
-    char* str = "0";
+char* int2str(int x) {
+    if (x == -2147483648) {
+        return "-2147483648";
+    }
+    bool y = false;
+    if (x == 0) {
+        return "0";
+    }
+    if (x < 0) {
+        y = true;
+        x = -x;
+    }
+    int len = 0;
+    int z = x;
+    while (z != 0) {
+        z /= 10;
+        len++;
+    }
+    char *str = (char*)malloc(len + y + 1);
+    str[len + y] = '\0';
+    for (int i = len + y - 1; i >= y; i--) {
+        str[i] = (x % 10) + '0';
+        x /= 10;
+    }
+    if (y) {
+        str[0] = '-';
+    }
     return str;
 }
+
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,5 +3,21 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+  int x = 1;
+  int z = 0;
+  if (*str == '-'){
+    x = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+  int count = 0;
+  while (*str != '\0') {
+    z = z * 10 + (*str - '0');
+    count += 1;
+    str++;
+    assert(x == -1 && z == -2147483648 || x == -1 && z >= 0 && count <= 9 || x == 1 && z >= 0);
+  }
+  assert(count > 0);
+  return z * x;
 }
